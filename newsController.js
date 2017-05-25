9b5c4e41e49c723f66258003b31a3904723eb841
Cristina Iannone <iannonecristina@gmail.com>
2017-05-25 17:25:04 +0100

    Adds newsController.js

---
+++ newsController.js
@@ -0,0 +1,10 @@
+var element = document.getElementById('app');
+console.log(element);
+
+function newsController() {
+this.newStories =
+}
+
+newsController.prototype.displayNews = function () {
+  document.getElementById("app") = this.newStories.APIoutput();
+};
